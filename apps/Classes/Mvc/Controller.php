e5cab1272fa78adb4c80f978b78daa46dc5c1264
Marcus Biesioroff <marcus@biesioroff.com>
2020-08-26 04:23:41 +0200

    Next commit, let's hope it's working

--- apps/Classes/Mvc/Controller.php
+++ apps/Classes/Mvc/Controller.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace IcarosNet\LastHammer\Mvc;
+use IcarosNet\LastHammer\Gen\Vars;
+
 class Controller
 {
     private static $instance = null;
@@ -20,13 +22,12 @@ class Controller
         $output     = 1;
         $process    = null;
         $template   = null;
-        $mvc        = &\CoreApp::$oclass['MVC'];
         $stuser     = \CoreApp::$ovars['USER']['LOGGED'];
         $isdata     = \CoreApp::$ovars['SYS']['HTTP']['STATE'];
         $isdatatype = \CoreApp::$ovars['SYS']['HTTP']['METHOD'];
         $iscron     = \CoreApp::$ovars['SYS']['SERVICE']['CRON'];
         $iswser     = \CoreApp::$ovars['SYS']['SERVICE']['WEBSER'];
-        $mvc['LANG']->routeLang();
+        Lang::_getInstance()->routeLang();
         ($stuser ? ($isdata ? ('GET' == $isdatatype ? $template = 3 : $process = 1) : $template = 1) :
             ($isdata && 'POST' == $isdatatype ? ($iscron ? list($process, $output) = [1, null] :
                 ($iswser ? list($process, $output) = [1, 2] : $process = 1)) :
@@ -38,9 +39,11 @@ class Controller
         # - all Proccess Run on the same enviroment.
         # - output can set to: 1 HTML format, 2 JSON
 
-        (null === $process) ?: $mvc['PROCESS']->runProcess();                                                                                                                                                     //Model
-        (null === $template) ?: $mvc['TEMPLATE']->getView($template);                                                                                                                                             //View
-        (null === $output) ? \IcarosNet\LastHammer\Gen\Vars::_getInstance()->expVariable(true, true, false, true, \CoreApp::$ovars, [$stuser, $isdata, $isdatatype, $iscron, $iswser]) : $mvc['OUTPUTDATA']->showOutput($output); //Output
+        (null === $process) ?: Process::_getInstance()->runProcess();                                                                                                                                                     //Model
+        (null === $template) ?: Template::_getInstance()->getView($template);                                                                                                                                             //View
+        (null === $output) ? Vars::_getInstance()
+            ->expVariable(true, true, false, true, \CoreApp::$ovars, [$stuser, $isdata, $isdatatype, $iscron, $iswser]) : OutputData::_getInstance()
+            ->showOutput($output); //Output
 
         # ! ::::::::WARNING:::::::: Do not touch this section ::::::::WARNING::::::::
     }
